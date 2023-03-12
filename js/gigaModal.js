e747012621b72c2890fb9aeeffe17ef9e76a7d32
Oleg Strekachev <olegstrekachev@gmail.com>
2023-03-12 15:44:00 -0600

    Added touch functionality for mobile devices

--- js/gigaModal.js
+++ js/gigaModal.js
@@ -18,14 +18,14 @@ function GigaModalGallery(el, index) {
     document.body.insertAdjacentHTML("beforeend", `<div id ='gigaModal' class="gigaModalSection">
     <div class="gigaOverlay"> </div>
     <div class="gigaLargePreview">
-            <div class="gigaCloseButton">
-                <button> </button>
-            </div>
             <div class="button Previous">
                 <button></button>
             </div>
             <!-- Gallery Image -->
             <div class="image">
+                <div class="gigaCloseButton">
+                <button> </button>
+                </div>
                 <img id="modalImg" src="" alt="">
             </div>
             <div class="button Next">
@@ -60,6 +60,12 @@ function GigaModalGallery(el, index) {
         buttonPrevious: document.querySelector('.button.Previous button'),
         buttonNext: document.querySelector('.button.Next button'),
         buttonEsc: document.querySelector('.gigaModalSection .gigaCloseButton'),
+        deviceType: undefined,
+        pointerDown: 'up',
+        lastPos: undefined,
+        pointerDownStamp: undefined,
+        pointerUpStamp: undefined,
+        previousMoveDistance: [],
 
         //functions
 
@@ -76,6 +82,21 @@ function GigaModalGallery(el, index) {
                     item.classList.remove('active');
                 })
             modalDeclarations.carouselItem[modalDeclarations.index].classList.add('active');
+        },
+
+        scrollOnClick: function () {
+            modalDeclarations.carouselItem.forEach((item, index) => {
+                 item.addEventListener('click', () => {
+                     modalDeclarations.index = index;
+                     modalDeclarations.scrollImagetoVPCenter(item, index);
+                 })
+             })
+        },
+
+        initializeState: function () {
+            modalDeclarations.previousMoveDistance = [];
+            modalDeclarations.pointerDown = 'up';
+            modalDeclarations.modalImage.style.opacity =  1;
         },
 
     
@@ -144,87 +165,121 @@ function GigaModalGallery(el, index) {
             modalDeclarations.scrollImagetoVPCenter(modalDeclarations.carouselItem[modalDeclarations.index]);
     
         // Carousel slide on click
-        
-           modalDeclarations.carouselItem.forEach((item, index) => {
-                item.addEventListener('click', () => {
-                    modalDeclarations.index = index;
-                    modalDeclarations.scrollImagetoVPCenter(item, index);
-                })
-            })
 
-        // Pointer events
-        // This one scrubs the carousel mini-gallery
 
-        let pointerDown;
-   
+    
+        // Pointer events related to navigation of the carousel mini-gallery on the bottom
+
         modalDeclarations.carousel.addEventListener('pointerdown', e => {
+
+        // Disables default drag-and-drop behaviour
+
             e.preventDefault();
-            console.log(e);
-            pointerDown = true;
+
+        // Differentiates clicks from slides
+
+            modalDeclarations.pointerDownStamp = e.timeStamp
+            modalDeclarations.pointerDown = 'down';
             modalDeclarations.carousel.style.transition = `transform 0s`;
         })
 
         modalDeclarations.carousel.addEventListener('pointerup', e => {
-            pointerDown = false;
+            modalDeclarations.pointerUpStamp = e.timeStamp;
+
+            if (modalDeclarations.pointerUpStamp - modalDeclarations.pointerDownStamp < 200) {
+                modalDeclarations.scrollOnClick();
+            };
+
+            modalDeclarations.pointerDown = 'up';
             modalDeclarations.carousel.style.transition = `transform 0.4s ease-in-out`;
             modalDeclarations.scrollImagetoVPCenter(modalDeclarations.carouselItem[modalDeclarations.index]);
-            lastPos = 'undefined';
+            modalDeclarations.lastPos = undefined;
         })
-        modalDeclarations.carousel.addEventListener('pointercancel', e => {
-        })
-
-        let lastPos;
-
+   
+      
         modalDeclarations.carousel.addEventListener('pointermove', e => {
               
-                if (typeof lastPos !== "undefined" && pointerDown) {
-                    let moveDIstance = e.clientX - lastPos;
-                    lastPos = e.clientX;
-                    console.log('distance', moveDIstance);
-                    
-                modalDeclarations.carousel.style.transform = `translateX(${Number(window.getComputedStyle(modalDeclarations.carousel).transform.split(',')[4]) + moveDIstance}px)`;
-
+            if (typeof modalDeclarations.lastPos !== "undefined" && modalDeclarations.pointerDown == 'down' ) {
+                let moveDistance = e.clientX - modalDeclarations.lastPos;
+                modalDeclarations.lastPos = e.clientX;
+                modalDeclarations.carousel.style.transform = `translateX(${Number(window.getComputedStyle(modalDeclarations.carousel).transform.split(',')[4]) + moveDistance}px)`;
                 const middleOfTheViewport = document.documentElement.clientWidth / 2; 
 
                 modalDeclarations.carouselItem.forEach((item, index) => {
-                   
-                    const getLeftEdgeItem = item.getBoundingClientRect().left;
-                    const carouselItemWidth = item.getBoundingClientRect().width;
+                const getLeftEdgeItem = item.getBoundingClientRect().left;
+                const carouselItemWidth = item.getBoundingClientRect().width;
 
-                    if (Math.abs(getLeftEdgeItem + carouselItemWidth /2  - middleOfTheViewport) < 10) {
-                        console.log('Bingo', index)
-                        modalDeclarations.index = index;
-                        modalDeclarations.modalImage.src =  modalDeclarations.carouselItem[modalDeclarations.index].getAttribute('src');
-                        modalDeclarations.activeToggle();
-                    } 
-                
-                })
-
-                    } else {
-                        lastPos = e.clientX;
-                    }
-               
+                if (Math.abs(getLeftEdgeItem + carouselItemWidth /2  - middleOfTheViewport) < carouselItemWidth / 3 &&  modalDeclarations.index !== index) {
+                    modalDeclarations.index = index;
+                    modalDeclarations.modalImage.src =  modalDeclarations.carouselItem[modalDeclarations.index].getAttribute('src');
+                    modalDeclarations.activeToggle();
+                } 
+            })} else {
+                modalDeclarations.lastPos = e.clientX;
+            }
         })
 
+        // Pointer events related to navigation of the big preview image.
 
-        // Pointer events
-        // This one adds main preview swipe functionality
+        // Enables touch swipe behaviour only when touch is supported by the device
 
-        
-       
+            window.addEventListener("pointerdown", detectInputType);
+                function detectInputType(event) {
+                    switch(event.pointerType) {
+                        case "touch":
+                            modalDeclarations.deviceType = 'touch';
+                            /* touch input detected */
+                            break;
+                    }
+                }
+
+            modalDeclarations.modalImage.addEventListener('pointerdown', e => {
+                 e.preventDefault();
+                modalDeclarations.pointerDown = 'down';
+            });
+
+            modalDeclarations.modalImage.addEventListener('pointermove', e => {
+
+
+                if (modalDeclarations.pointerDown === 'down' && modalDeclarations.deviceType == 'touch') {
+                    console.log('pointer down state');
+                    if (typeof modalDeclarations.lastPos== 'undefined') {
+                        modalDeclarations.lastPos= e.clientX;
+                    }
+                    let pointerMoveDistance = e.clientX - modalDeclarations.lastPos;
+                    modalDeclarations.previousMoveDistance.push(pointerMoveDistance);
+                    modalDeclarations.lastPos= e.clientX;
+                    const moveDistanceSum = modalDeclarations.previousMoveDistance.reduce(
+                        (accumulator, currentValue) => {return accumulator + currentValue}, 0);
+                    // modalDeclarations.modalImage.style.opacity =  1 - (Math.abs(moveDistanceSum) * 0.01);
+                        if (moveDistanceSum > window.innerWidth / 10) {
+                            modalDeclarations.initializeState();
+                            modalDeclarations.slideRight();
+                    
+                        } else if (moveDistanceSum < - window.innerWidth / 10) {
+                            modalDeclarations.initializeState();
+                            modalDeclarations.slideLeft();
+                        }
+                }
+            });
+
+            modalDeclarations.modalImage.addEventListener('pointerup', e => {
+                modalDeclarations.lastPos= undefined;
+                modalDeclarations.pointerDown = 'up';
+                modalDeclarations.modalImage.style.opacity =  1;
+            });
+
         // Passing event into key controls function
 
             document.addEventListener('keydown', (e) => {
                 modalDeclarations.bindKeyControls(e);
             });
 
-        
          // Adding click navigation functionality
 
             modalDeclarations.buttonPrevious.addEventListener('click', modalDeclarations. slideLeft);
             modalDeclarations.buttonNext.addEventListener('click', modalDeclarations. slideRight);
             modalDeclarations.buttonEsc.addEventListener('click', modalDeclarations.removeModal);
-
     }
 
     this.modalExecution()
